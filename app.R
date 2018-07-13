5ac4d0b38046b2d922428ec668f75141fdf251e5
Julianna Alvord <jalvord@smith.edu>
2018-07-13 17:20:34 -0400

    sources from dfs.R and added uiOutput function to change the variable choices depending on which format of data the user chooses

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-pairwise <- read.csv("pairwise.csv")
+source("dfs.R")
 
 #Define UI
 ui <- fluidPage(
@@ -16,7 +16,11 @@ ui <- fluidPage(
       
       # Input: Choose dataset ----
       selectInput("dataset", "Which format do you want your data in?",
-                  choices = c("Pairwise")),
+                  choices = c("Individual", "Dyad", "Pairwise")),
+      
+      #Render UI in the server will display a selectInput() and let user choose variables 
+      #based on the format of data they
+      uiOutput("varControls"),
       
       # Button
       downloadButton("downloadData", "Download")
@@ -26,11 +30,16 @@ ui <- fluidPage(
     # Main panel for displaying outputs ----
     mainPanel(
       
-      tableOutput("table")
+      tabsetPanel(type = "tabs",
+                  tabPanel("View Data", tableOutput("table")),
+                  tabPanel("Explore Variables"),
+                  tabPanel("Data Formats")
+      )
       
     )
     
   )
+  
 )
 
 server <- function(input, output) {
@@ -38,14 +47,25 @@ server <- function(input, output) {
   # Reactive value for selected dataset ----
   datasetInput <- reactive({
     switch(input$dataset,
-           # "Individual" = rock,
-           # "Dyad" = pressure,
-           "Pairwise" = pairwise)
+           "Individual" = df_indiv,
+           "Dyad" = df_dyad,
+           "Pairwise" = df_pair)
+  })
+  
+  varInput <- reactive({
+   input$vars
   })
   
   # Table of selected dataset ----
   output$table <- renderTable({
-    head(datasetInput())
+    head(datasetInput() %>%
+      select(varInput()))
+  })
+  
+  output$varControls <- renderUI({
+    whichone <- datasetInput()
+    selectInput("vars", "Which variables do you want included?",
+                choices = names(whichone), multiple = TRUE, selected = "DYADID")
   })
   
   # Downloadable csv of selected dataset ----
